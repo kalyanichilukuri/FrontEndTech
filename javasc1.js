2e6b41b9b542dfa13a73ce6a42202cf7f81ad769
kalyanichilukuri <83775819+kalyanichilukuri@users.noreply.github.com>
2021-06-28 17:57:27 +0530

    Add files via upload

---
+++ javasc1.js
@@ -0,0 +1,6 @@
+//Write a program which consist of single line and multiline comments
+/*Write a program which consist of single line and multiline comments
+multi line comments in java script*/
+let x=9;
+let y= 10+x;
+document.writeln("addition of x and y is",y); 
